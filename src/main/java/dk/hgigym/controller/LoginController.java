94ebf90083147bfb5a78bf115473547299703239
a5vTech <jesper2604@gmail.com>
2018-12-18 12:39:47 +0100

    FINAL VERSION V.2

--- src/main/java/dk/hgigym/controller/LoginController.java
+++ src/main/java/dk/hgigym/controller/LoginController.java
@@ -30,23 +30,23 @@ public class LoginController {
         return "login";
     }
 
-    @ResponseBody
-    @GetMapping("/createTestUser")
-    public String createTestUser() throws Exception {
-        User user = userRepository.findByEmail("instructor@hgi.dk");
-//        User user = new User();
-//        user.setEmail("janni@jensen-dahm.dk");
-//        user.setFirstName("Janni");
-//        user.setLastName("TESTER");
-//        user.setRole("ROLE_INSTRUCTOR");
-//        user.setEnabled(true);
-        user.setPassword(passwordEncoder.encode("1234"));
-        userRepository.save(user);
-
-//        mailSender.sendMail("jesper2604@gmail.com", "Test ", "This is message");
-//        mailSender.sendMail("mikkelkoksen@gmail.com", "Test ", "This is message");
-
-//        return "user created with email and pass \nEmail:" + user.getEmail() + "\nPass: 1234";
-        return "Send a mail";
-    }
+//    @ResponseBody
+//    @GetMapping("/createTestUser")
+//    public String createTestUser() throws Exception {
+//        User user = userRepository.findByEmail("instructor@hgi.dk");
+////        User user = new User();
+////        user.setEmail("janni@jensen-dahm.dk");
+////        user.setFirstName("Janni");
+////        user.setLastName("TESTER");
+////        user.setRole("ROLE_INSTRUCTOR");
+////        user.setEnabled(true);
+//        user.setPassword(passwordEncoder.encode("1234"));
+//        userRepository.save(user);
+//
+////        mailSender.sendMail("jesper2604@gmail.com", "Test ", "This is message");
+////        mailSender.sendMail("mikkelkoksen@gmail.com", "Test ", "This is message");
+//
+////        return "user created with email and pass \nEmail:" + user.getEmail() + "\nPass: 1234";
+//        return "Send a mail";
+//    }
 }
